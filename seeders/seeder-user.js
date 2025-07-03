942ae2fe4507e8ae7f6b4b4b320c9452e209dcc8
HoiNamIT <nnam43367@gmail.com>
2025-07-03 18:22:03 +0700

    create all model and migration data

--- seeders/seeder-user.js
+++ seeders/seeder-user.js
@@ -4,9 +4,13 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
    return queryInterface.bulkInsert('Users', [
       {
+        email:'xample@example.com',
+        password:'123456',
         firstName: 'John',
-        lastName: 'Doe',
-        email: 'example@example.com',
+        lastName: 'Does',
+        address: 'VN',
+        gender: 1,
+        rodeId:'rode 1',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
